448c437bd67aa288c8016a6b8c6adad66c55b2c1
Seraph <seraph.lesabre@live.com>
2012-05-21 16:12:45 -0700

    Added basic Drink Database manager

---
+++ src/coffeebot/client/drinkDatabaseManager.java
@@ -0,0 +1,15 @@
+package coffeebot.client;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class drinkDatabaseManager {
+	int id = 0;
+	public static final String DATABASE_NAME = "drinkDatabase";
+
+}
